8f39e9f43cc473f8a4b96a74130ef5de7463640a
WUMANHO <wumanho@tutanota.com>
2022-06-23 21:54:14 +0800

    use reduce

--- src/context/user.context.jsx
+++ src/context/user.context.jsx
@@ -1,4 +1,4 @@
-import {createContext, useState, useEffect} from "react";
+import {createContext,  useEffect, useReducer} from "react";
 import {createUserDocumentFromAuth, onAuthStateChangeListener} from "../utils/firebase/firebase.util";
 
 export const UserContext = createContext({
@@ -6,9 +6,36 @@ export const UserContext = createContext({
   setCurrentUser: () => null
 })
 
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: 'SET_CURRENT_USER'
+}
+
+// reducer
+const userReducer = (state, action) => {
+  const {type, payload} = action
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        currentUser: payload
+      }
+    default:
+      throw new Error(`类型 ${type} 错误`)
+  }
+}
+
+const INITIAL_STATE = {
+  currentUser: null
+}
+
+
 export const UserProvider = ({children}) => {
-  const [currentUser, setCurrentUser] = useState(null)
-  const value = {currentUser, setCurrentUser}
+  const [state, dispatch] = useReducer(userReducer, INITIAL_STATE)
+  const {currentUser} = state
+
+  const setCurrentUser = (user) => {
+    dispatch({type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user})
+  }
+
   // 注册监听器
   useEffect(() => {
     const unsubscribe = onAuthStateChangeListener(async (user) => {
@@ -19,6 +46,8 @@ export const UserProvider = ({children}) => {
     })
     return unsubscribe
   }, [])
+
+  const value = {currentUser, setCurrentUser}
 
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>
 }
